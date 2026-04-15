87f6e813b9c30705cc85ba171f31d827b5b3d8e5
zhasmin-roumieh <zhasmin-roumieh@users.noreply.github.com>
2026-04-15 14:19:08 +0200

    Solidere photo: use Nejmeh Square (central plaza of Solidere)

--- src/data/wikiTitles.js
+++ src/data/wikiTitles.js
@@ -87,7 +87,7 @@ export const WIKI_TITLES = {
   raouche:             'Raouché',
   'bourj-hammoud':     'Bourj Hammoud',
   achrafieh:           'Achrafieh',
-  solidere:            'Solidere',
+  solidere:            'Nejmeh Square',
   'port-beirut':       'Port of Beirut',
   badaro:              'Badaro, Beirut',
   shatila:             'Shatila refugee camp',
